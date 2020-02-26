ffff4d038e23010baf731990c3a49cb56728d54d
wakaba.yotuya <cltpotjmdt@gmail.com>
2020-02-26 16:44:29 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
-Route::group(['middleware' => ['auth']], function () {Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);});
+Route::group(['middleware' => ['auth']], function () {Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+                                                      Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+});
