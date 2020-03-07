16953a26210a259f7f9bdbc20836e92b68fb5540
wakaba.yotuya <cltpotjmdt@gmail.com>
2020-03-07 10:53:00 +0000

    add favorite 20200307

--- resources/views/micropost_favorite/favorite_button.blade.php
+++ resources/views/micropost_favorite/favorite_button.blade.php
@@ -1,9 +1,9 @@
-@if (Auth::user()->is_favoriting($user->id))
-    {!! Form::open(['route' => ['favorites.unfavorite', $user->id], 'method' => 'delete']) !!}
+@if (Auth::user()->is_favoriting($microposts->id))
+    {!! Form::open(['route' => ['favorites.unfavorite', $microposts->id], 'method' => 'delete']) !!}
     {!! Form::submit('Unfavorite', ['class' => "btn btn-light btn-sm"]) !!}
     {!! Form::close() !!}
 @else
-    {!! Form::open(['route' => ['favorites.favorite', $user->id]]) !!}
+    {!! Form::open(['route' => ['favorites.favorite', $microposts->id]]) !!}
     {!! Form::submit('Favorite', ['class' => "btn btn-success btn-sm"]) !!}
     {!! Form::close() !!}
 @endif
